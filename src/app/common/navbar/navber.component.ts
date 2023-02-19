778fb692c084d15f23140f38781f7e5ae27c2db8
ryota <ryota0220sarawainu@ezweb.ne.jp>
2023-02-19 22:59:18 +0900

    ヘッダーコンポーネント追加

---
+++ src/app/common/navbar/navber.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navber.component.html',
+  styleUrls: ['./navber.component.scss']
+})
+export class NavComponent {
+}
